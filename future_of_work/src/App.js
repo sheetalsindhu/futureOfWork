0aa7ed5e7fc4e0ac0d549f28325111b397f9065c
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-11-03 13:51:16 +0530

    src

---
+++ future_of_work/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Card from "./components/Card/Card";
+import BottomNav from "./components/bottom-nav/BottomNav";
+
+const workCard = {
+  title: "WORK",
+  subtitle: "from any corner of the world",
+  image:
+    "https://img.jakpost.net/c/2020/03/14/2020_03_14_89218_1584158212._large.jpg",
+  color: "#E5265A",
+};
+
+function App() {
+  return (
+    <div className="App">
+      <BottomNav />
+    </div>
+  );
+}
+
+export default App;
