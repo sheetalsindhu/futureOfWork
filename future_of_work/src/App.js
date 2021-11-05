834db9375764bba1a9ee75480389f132f80af5df
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-11-05 19:32:34 +0530

    newfile

--- future_of_work/src/App.js
+++ future_of_work/src/App.js
@@ -1,21 +1,23 @@
-import "./App.css";
-import Card from "./components/Card/Card";
-import BottomNav from "./components/bottom-nav/BottomNav";
-import Menu from "./components/menu/Menu";
-
-const workCard = {
-  title: "WORK",
-  subtitle: "from any corner of the world",
-  image:
-    "https://img.jakpost.net/c/2020/03/14/2020_03_14_89218_1584158212._large.jpg",
-  color: "#E5265A",
-};
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Menu />
-      {/* <BottomNav /> */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
