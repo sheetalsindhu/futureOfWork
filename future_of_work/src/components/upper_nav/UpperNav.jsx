cd08f44ba8b61f2b5007078bf7c78d9bd5dc2b6e
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-11-10 19:22:08 +0530

    nav bar changes

--- future_of_work/src/components/upper_nav/UpperNav.jsx
+++ future_of_work/src/components/upper_nav/UpperNav.jsx
@@ -1,9 +1,23 @@
 import styles from "./upperNav.module.css";
+// import { link } from "react-router-dom";
+import { RiArrowLeftSLine } from "react-icons/ri";
+import { GiHamburgerMenu } from "react-icons/gi";
+
 function UpperNav() {
   return (
-    <>
-      <div className={styles.upper_div}>hello</div>
-    </>
+    <div className={styles.navbar}>
+      <div className={styles.first}>
+        <a className={styles.link}>
+          <RiArrowLeftSLine className={styles.icon} />
+        </a>
+        <p className={styles.heading}>Worklyf</p>
+      </div>
+      <div>
+        <a className={styles.humburger}>
+          <GiHamburgerMenu className={styles.icon} />
+        </a>
+      </div>
+    </div>
   );
 }
 
