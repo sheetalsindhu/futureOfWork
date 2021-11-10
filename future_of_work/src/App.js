00b9dae8567f18758228f59d8f2787172d4c2f39
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-11-11 00:22:49 +0530

    path set and back arrow routing

--- future_of_work/src/App.js
+++ future_of_work/src/App.js
@@ -9,17 +9,41 @@ import Workpass from "./components/workpass/Workpass";
 import Worklyf from "./components/worklyf/Worklyf";
 import Slider from "./components/Slider/Slider";
 import Team from "./components/team/Team";
+import Homepage from "./components/Homepage/Homepage";
+import Learn from "./components/Learn/Learn";
+import Jobs from "./components/Jobs/Jobs";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/Card/:id" exact>
+        <Route path="/">
+          <Homepage />
+        </Route>
+        <Route path="/Learn">
+          <Learn />
+        </Route>
+        <Route path="/Jobs">
+          <Jobs />
+        </Route>
+
+        <Route path="/worklyf" exact>
+          <Worklyf />
+        </Route>
+        <Route path="/card/:id" exact>
           <Workpass />
         </Route>
 
-        <Route path="/Cards" exact>
+        <Route path="/cards" exact>
           <Card />
+        </Route>
+
+        <Route path="/tribe" exact>
+          <Tribe />
+        </Route>
+
+        <Route path="/team">
+          <Team />
         </Route>
       </Switch>
     </div>
@@ -27,3 +51,5 @@ function App() {
 }
 
 export default App;
+
+import "./App.css";
