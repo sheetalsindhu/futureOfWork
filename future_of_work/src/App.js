a8a597fd48a575860e8b9ade505ba2116e469185
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-11-10 19:25:39 +0530

    new changes

--- future_of_work/src/App.js
+++ future_of_work/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
-import { useState } from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import Menu from "./components/menu/Menu";
 import Tribe from "./components/tribe/Tribe";
@@ -10,47 +9,19 @@ import Workpass from "./components/workpass/Workpass";
 import Worklyf from "./components/worklyf/Worklyf";
 import Slider from "./components/Slider/Slider";
 import Team from "./components/team/Team";
-import BottomNav from "./components/bottom-nav/BottomNav";
-
-import Data from "./data.json";
 
 function App() {
-  const [workPass, setworkPass] = useState(Data);
-
   return (
     <div className="App">
-      {/* <Slider />
-      <br></br>
-      <Worklyf />
-      <br></br>
-      <Menu />
-      <br></br>
-      <Tribe />
-      <br></br> */}
-
-      {/* <Card />
-      <br></br> */}
-      {/* 
-      <Thankyou />
-      <br></br>
-      <Team /> */}
-
-      <BottomNav />
-      <br></br>
-      <div className="data">
-        {workPass.map(
-          (el) => (
-            (<Card key={el.title} data={el} />),
-            (<Workpass key={el.title} data={el} />)
-          )
-        )}
-      </div>
-
-      {/* <Switch>
-        <Route path="/Workpass/:id">
+      <Switch>
+        <Route path="/Card/:id" exact>
           <Workpass />
         </Route>
-      </Switch> */}
+
+        <Route path="/Cards" exact>
+          <Card />
+        </Route>
+      </Switch>
     </div>
   );
 }
