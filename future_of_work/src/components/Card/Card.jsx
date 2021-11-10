6ba3540eb7a59b9dc1b4df86e11a8d276eb1fb2c
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-11-10 19:22:38 +0530

    card done

--- future_of_work/src/components/Card/Card.jsx
+++ future_of_work/src/components/Card/Card.jsx
@@ -2,18 +2,12 @@ import styles from "./Card.module.css";
 import UpperNav from "../upper_nav/UpperNav";
 import BottomNav from "../bottom-nav/BottomNav";
 // import { Link } from "react-router-dom/cjs/react-router-dom.min";
-import { useState } from "react";
+
 import Workpass from "../workpass/Workpass";
+import Data from "../../data.json";
+import { Link } from "react-router-dom";
 
-const Card = ({ data }) => {
-  const [cardData, setCardData] = useState({});
-
-  const handleClick = (e) => {
-    setCardData(e);
-  };
-
-  
-
+const Card = () => {
   return (
     <>
       <UpperNav />
@@ -21,28 +15,23 @@ const Card = ({ data }) => {
       <div className={styles.main}>
         <h1>Select Your Pass</h1>
 
-        {data.map((data) => (
-          <div
-            onClick={() => {
-              handleClick(data);
-            }}
-            className={styles.card}
-          >
-            <div className={styles.details}>
-              <h1 className={styles.title}>{data.title}</h1>
-              <ul>
-                {data.features.map((el) => (
-                  <li> - {el}</li>
-                ))}
-              </ul>
+        {Data.map((data) => (
+          <Link to={`Card/${data.id}`}>
+            <div className={styles.card}>
+              <div className={styles.details}>
+                <h1 className={styles.title}>{data.title}</h1>
+                <ul>
+                  {data.features.map((el) => (
+                    <li> - {el}</li>
+                  ))}
+                </ul>
+              </div>
+              <div className={styles.duration}>
+                <p>{data.duration}</p>
+              </div>
             </div>
-            <div className={styles.duration}>
-              <p>{data.duration}</p>
-            </div>
-          </div>
+          </Link>
         ))}
-
-        <Workpass data={cardData} />
       </div>
     </>
   );
